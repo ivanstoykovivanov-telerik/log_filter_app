138a1ead723d8d250948e4b8e93e9d7b988d75e8
Ivan Ivanov <Ivan.Ivanov@softwareag.com>
2019-06-25 14:07:43 +0300

    Get logs from the second request successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,25 +19,25 @@ function search(text: string, pipe: PipeTransform): Country[] {
 
 const COUNTRIES: Country[] = [
   {
-    name: 'Russia',
+    name: 'Log1',
     flag: 'f/f3/Flag_of_Russia.svg',
     area: 17075200,
     population: 146989754
   },
   {
-    name: 'Canada',
+    name: 'Log2',
     flag: 'c/cf/Flag_of_Canada.svg',
     area: 9976140,
     population: 36624199
   },
   {
-    name: 'United States',
+    name: 'Log3',
     flag: 'a/a4/Flag_of_the_United_States.svg',
     area: 9629091,
     population: 324459463
   },
   {
-    name: 'China',
+    name: 'Log4',
     flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
     area: 9596960,
     population: 1409517397
@@ -65,7 +65,7 @@ export class AppComponent {
     );
     
     this.getLogs(); 
-    
+
   }
 
   getLogs(){
--- src/app/log.service.ts
+++ src/app/log.service.ts
@@ -1,11 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+ 
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'Basic ' + btoa(`${username}:${password}`)
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogService {
+  
+  // FIRST 
   private baseUrl : string = "https://adamosoeedev.adamos-dev.com/inventory/managedObjects?pageSize=100&type=c8y_Application_6447&withTotalPages=true"; 
+  private second : string =   "https://adamosoeedev.adamos-dev.com/event/events?dateFrom=2019-06-06T16:33:52%2B03:00&dateTo=2019-06-06T16:43:52%2B03:00&fragmentType=c8y_Instance&fragmentValue=apama-oeeapp-scope-t44680917-deployment-7894c5b96-mnclx&pageSize=100&source=3637&type=c8y_LogfileRequest" ; 
+  private secondRequest: string = "https://adamosoeedev.adamos-dev.com/event/events"; 
+  private parameterToSecondRequest: string; 
+  private dateFrom = "2019-06-06T16:33:52%2B03:00"; 
+  private dateTo = "2019-06-06T16:43:52%2B03:00" ; 
+  private fragmentType: string = "c8y_Instance";
+  private fragmentValue :string = "apama-oeeapp-scope-t44680917-deployment-7894c5b96-mnclx";    
+  private pageSize: number = 100; 
+  private source: number = 3637;
+  private type = "c8y_LogfileRequest"; 
+
+  private readyUrl : string = `${this.baseUrl}events?dateFrom=${this.dateFrom}&dateTo=${this.dateTo}&fragmentType=${this.fragmentType}&fragmentValue=${this.fragmentValue}&pageSize=${this.pageSize}&source=${this.source}&type=${this.type}`; 
+  
+  
+  //SECOND
+ // private baseUrl : string =  "https://adamosoeedev.adamos-dev.com/event/events?dateFrom=2019-06-06T16:33:52%2B03:00&dateTo=2019-06-06T16:43:52%2B03:00&fragmentType=c8y_Instance&fragmentValue=apama-oeeapp-scope-t44680917-deployment-7894c5b96-mnclx&pageSize=100&source=3637&type=c8y_LogfileRequest"; 
+  //private baseUrl : string = "https://adamosoeedev.adamos-dev.com/inventory/"; 
 
 
   constructor(
@@ -13,11 +42,28 @@ export class LogService {
   ) { }
 
   getLogs(){
-    this.httpClient.get(this.baseUrl).subscribe((res)=>{
-      console.log(res);
-    })
+    this.httpClient.get(this.baseUrl, httpOptions).subscribe(
+      (res : any) => {
+      // console.log(res);
+      // console.log(res.managedObjects[0].c8y_Subscriptions);
+      
+      console.log(res.managedObjects[0].c8y_Subscriptions.t44680917.instances);
+     // this.fragmentValue = res.managedObjects[0].c8y_Subscriptions.t44680917.instances ; 
+      console.log(this.fragmentValue);
+                              //https://adamosoeedev.adamos-dev.com/event/events?dateFrom=2019-06-06T16:33:52%2B03:00&dateTo=2019-06-06T16:43:52%2B03:00&fragmentType=c8y_Instance&fragmentValue=apama-oeeapp-scope-t44680917-deployment-7894c5b96-mnclx&pageSize=100&source=3637&type=c8y_LogfileRequest
+      let secondRequestN =   `${this.secondRequest}?dateFrom=${this.dateFrom}&dateTo=${this.dateTo}&fragmentType=${this.fragmentType}&fragmentValue=${this.fragmentValue}&pageSize=${this.pageSize}&source=${this.source}&type=${this.type}` ; 
+      console.log(secondRequestN);
+
+      //Second
+      this.httpClient.get(secondRequestN, httpOptions).subscribe(
+        (res2: any) => { 
+          console.log("Second request: ");
+          console.log(res2);
+          console.log("EVENTS:  ");
+          console.log(res2.events[0]);
+    }); 
+  })
   }
 
-
 }
-  
+
